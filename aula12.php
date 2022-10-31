3c383a05edda7983f3bb078f3da39a815ea1fdba
VitorMoreiraAraujo <vitormoreira.araujo@gmail.com>
2022-10-31 20:06:17 -0300

    aula12 parte2

--- aula12.php
+++ aula12.php
@@ -10,8 +10,8 @@
 		Formulário
 	</h1>
 	<form name="cadastro" method="get" action="pagina_form.php">
-		Nome:<input type="text" name="nome"> <br><br>
-		Senha: <input type="password" name="senha"> <br><br>
+		Nome:<input type="text" name="nome" maxlength="20" size="20"> <br><br>
+		Senha: <input type="password" name="senha" maxlength="20" size="20"> <br><br>
 		<input type="checkbox" name="termouso" value="termo">
 		Aceito o termo de uso <br><br>
 		<input type="radio" name="sexo" value="fem">Feminino
@@ -24,6 +24,50 @@
 		<input type="reset" name="LIMPAR">
 		&nbsp;&nbsp;&nbsp;
 		<input type="submit" name="ENVIAR">
+		&nbsp;&nbsp;&nbsp;
+		<a href="#" onclick="document.cadastro.reset()">
+			LIMPAR
+		</a>
+		<br><br>
+		<button onclick="windowy.location.href=´https://www.youtube.com/´">
+			Youtube
+		</button>
+		<input type="submit" name="youtube" onclick="windowy.location.href=´https://www.youtube.com/´">
+		<br><br>
+		<button onclick="windowy.location.reload()">
+			Atualizar página
+		</button>
+		&nbsp;&nbsp;&nbsp;
+		<button onclick="windowy.print()">
+			Imprimir
+		</button>
+		&nbsp;&nbsp;&nbsp;
+		<button onclick="windowy.history.back()">
+			Voltar
+		</button>
+		<br><br>
+		Animal:
+		<select name="animais">
+			<option value="elef">
+				Elefante
+			</option>
+			<option value="hpp">
+				Hipopótamo
+			</option>
+			<option value="rino">
+				Rinoceronte
+			</option>
+			<option value="dino" selected="true">
+				Dinossauro
+			</option>
+		</select>
+		<br><br>
+		Mensagem <br>
+		<textarea name="mens" cols="40" rows="15"></textarea>
 	</form>
 </body>
 </html>
+
+<?php 
+//imagem blob, varbinary, image, text - sem tamanho
+ ?>
---
+++ pagina_form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulário</title>
+</head>
+<body>
+	<h1>
+		Cadastrado !!!!!!!
+	</h1>
+</body>
+</html>
